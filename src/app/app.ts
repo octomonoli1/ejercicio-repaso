db0e2a99511d0caa99c60384bdc3a9be5205817a
vricheb973 <vricheb973@g.educaand.es>
2025-12-17 12:02:17 +0100

    añado vista html

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,37 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Empresa } from './models/empresa';
+import { Fct } from './models/fct';
+import { Alumno } from './models/alumno';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DatePipe],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit{
+
   protected readonly title = signal('ejercicio-repaso');
+  public empresa: Empresa | null = null;
+
+  ngOnInit(): void {
+    this.iniciar_app();
+  }
+
+  public iniciar_app(): void{
+    
+    let telefonos: Map<String, String > = new Map<String,String>();
+    telefonos.set("Casa", "988777222");
+    telefonos.set("Trabajo","778899110");
+
+    let alumno1: Alumno = new Alumno("1111111L","Pepito perez", new Date(), telefonos);
+    let alumno2: Alumno = new Alumno("2222222P","Juanita Sancho", new Date(), telefonos);
+    let fct1: Fct = new Fct(false, new Date("2026/01/12"), new Date("2026/04/15"), [alumno1, alumno2]);
+    let fct2: Fct = new Fct(true, new Date("2026/04/04"), new Date("2026/04/29"), [alumno1, alumno2]);
+
+    this.empresa = new Empresa("B0000123", "C/ Benajete 12", "La Lola", [fct1, fct2]);
+
+  }
 }
